a7609bb501633e735239378c996d91c3a34d5053
Tauqeer-flutter <tauqeer.flutter@tafsol.com>
2026-04-03 23:37:07 +0500

    Implement logout and forgot password functionality.

---
+++ lib/models/requests/email_request.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'email_request.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+Map<String, dynamic> _$EmailRequestToJson(EmailRequest instance) =>
+    <String, dynamic>{'email': instance.email};
